a5a98b3f514ae6fba2d686dee172c17f01161b11
ValentinaOvchinnikova <VA.Ovchinnikova@rambler.ru>
2025-01-02 00:02:42 +0300

    Delete add_list.py
    неправильный файл

--- add_list.py
+++
@@ -1,9 +0,0 @@
-olivye=input('Введите заголовок 1:')
-shuba=input('Введите заголовок 2:')
-alhogol=input('Введите заголовок 3:')
-spisok_zagolovkov=[olivye, shuba, alhogol]
-
-print('Заголовок 1:', olivye)
-print('Заголовок 2:', shuba)
-print('Заголовок 3:', alhogol)
-print('Список заголовков:', spisok_zagolovkov)
